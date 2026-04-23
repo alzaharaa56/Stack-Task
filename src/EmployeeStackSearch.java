68e836363dcc2186f09ccdb65c26e60b70818f0d
alzahara <shahdaljabri33@gmail.com>
2026-04-23 11:11:58 +0400

    Task 3:  Create a Stack and add at least 6 employee action

---
+++ src/EmployeeStackSearch.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+import java.util.logging.MemoryHandler;
+
+public class EmployeeStackSearch {
+    public static void main (String[] args){
+
+        Stack<String> employeeActions = new Stack<>();
+
+employeeActions.push("Login - User 1");
+employeeActions.push("Update Profile - User 2");
+employeeActions.push("Delete File - User 3");
+employeeActions.push("Upload Document - User 4");
+employeeActions.push("Logout - User 1");
+employeeActions.push("Change Password - User 5");
+
+
+    }
+}
