84da5ca15f9cae5c9758c26d6d1f49acf3c08e9d
alzahara <shahdaljabri33@gmail.com>
2026-04-27 15:00:23 +0400

    add task 16

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 
 public class MinStack {
+
     private static class Pair {
         int value;
         int minSoFar;
@@ -15,6 +16,7 @@ public class MinStack {
 
     private Stack<Pair> mainStack1 = new Stack<>();
 
+
     private Stack<Integer> mainStack2 = new Stack<>();
     private Stack<Integer> minStack2 = new Stack<>();
 
@@ -26,6 +28,7 @@ public class MinStack {
         display1();
     }
 
+
     public void pop1() {
         if (mainStack1.isEmpty()) {
             System.out.println("Stack1 empty, cannot pop.");
@@ -36,6 +39,7 @@ public class MinStack {
         display1();
     }
 
+
     public void peek1() {
         if (mainStack1.isEmpty()) {
             System.out.println("Stack1 empty, cannot peek.");
@@ -45,6 +49,7 @@ public class MinStack {
         display1();
     }
 
+
     public void getMin1() {
         if (mainStack1.isEmpty()) {
             System.out.println("Stack1 empty, no min.");
@@ -53,9 +58,11 @@ public class MinStack {
         System.out.println("Min1: " + mainStack1.peek().minSoFar);
     }
 
+
     public void display1() {
         System.out.println("MainStack1: " + mainStack1);
     }
+
 
     public void push2(int value) {
         mainStack2.push(value);
@@ -80,6 +87,7 @@ public class MinStack {
         display2();
     }
 
+
     public void peek2() {
         if (mainStack2.isEmpty()) {
             System.out.println("Stack2 empty, cannot peek.");
@@ -89,17 +97,40 @@ public class MinStack {
         display2();
     }
 
+
+    public void getMin2() {
+        if (minStack2.isEmpty()) {
+            System.out.println("Stack2 empty, no min.");
+            return;
+        }
+        System.out.println("Min2: " + minStack2.peek());
+    }
+
+
     public void display2() {
         System.out.println("MainStack2: " + mainStack2);
         System.out.println("MinStack2: " + minStack2);
     }
 
+    public static void main(String[] args) {
+        MinStack ms = new MinStack();
 
 
-
-
-
-
-
-
+        ms.push1(5); ms.push2(5);
+        ms.push1(3); ms.push2(3);
+        ms.push1(7); ms.push2(7);
+        ms.getMin1(); ms.getMin2();
+        ms.pop1(); ms.pop2();
+        ms.getMin1(); ms.getMin2();
+        ms.push1(2); ms.push2(2);
+        ms.push1(8); ms.push2(8);
+        ms.peek1(); ms.peek2();
+        ms.getMin1(); ms.getMin2();
+        ms.pop1(); ms.pop2();
+        ms.pop1(); ms.pop2();
+        ms.getMin1(); ms.getMin2();
+        ms.pop1(); ms.pop2();
+        ms.pop1(); ms.pop2(); // edge case: popping until empty
+        ms.getMin1(); ms.getMin2(); // edge case: min from empty
+    }
 }
