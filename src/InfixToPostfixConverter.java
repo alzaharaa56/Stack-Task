c117ff3007ee0a996d8a4fee320a7c157287b18a
alzahara <shahdaljabri33@gmail.com>
2026-04-27 09:23:04 +0400

    Apply arithmetic operation

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -17,7 +17,19 @@ public class InfixToPostfixConverter {
         return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '^';
     }
 
-
+    private static double applyOperation(double a, double b, char operator) {
+        switch (operator) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/':
+                if (b == 0) throw new ArithmeticException("Division by zero");
+                return a / b;
+            case '%': return a % b;
+            case '^': return Math.pow(a, b);
+            default: throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
 
 
 
