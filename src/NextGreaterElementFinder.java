151cafac693cefb87efa24719cf69dffac4b4b86
alzahara <shahdaljabri33@gmail.com>
2026-04-27 10:13:43 +0400

    Find next greater element using stack (O(n))

---
+++ src/NextGreaterElementFinder.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+import java.util.Arrays;
+
+public class NextGreaterElementFinder {
+
+
+    public static int[] findNextGreaterElement(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+
+
+    }
+}
