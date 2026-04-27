b7a16d4968bc8d0cd7a22e7dcefffd232fdb14a8
alzahara <shahdaljabri33@gmail.com>
2026-04-27 12:29:26 +0400

    Check if queue is empty

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -38,6 +38,10 @@ public class QueueUsingTwoStacks {
         showState();
         return val;
     }
+    public boolean isEmpty() {
+        return inputStack.isEmpty() && outputStack.isEmpty();
+    }
+
 
 
 
