d0a1a6f29f4a069ca5f532658efeea8413f5cf37
alzahara <shahdaljabri33@gmail.com>
2026-04-27 10:57:06 +0400

    if stack is not empty, then

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -13,10 +13,29 @@ public class NextGreaterElementFinder {
     {
         Stack<Integer> s = new Stack<>();
         int nge[] = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--)
+        {
 
-
+            if (!s.empty())
+            {
+                while (!s.empty()
+                        && s.peek() <= arr[i])
+                {
+                    s.pop();
+                }
+            }
+            nge[i] = s.empty() ? -1 : s.peek();
+            s.push(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i] +
+                    " --> " + nge[i]);
     }
+
+
+
 }
+
 
 
 
