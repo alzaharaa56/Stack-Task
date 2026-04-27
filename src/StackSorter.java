042fd87c80b30845a02cc66ae0b6f6e516cd77b0
alzahara <shahdaljabri33@gmail.com>
2026-04-27 15:47:35 +0400

    add task 17

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -47,11 +47,52 @@ public class StackSorter {
         }
     }
 
-
-
-
-
-        }
+    public static void displayStack(Stack<Integer> stack, String name) {
+        System.out.println(name + ": " + stack);
     }
+
+    public static void main(String[] args) {
+
+        Stack<Integer>[] testStacks = new Stack[5];
+
+        testStacks[0] = new Stack<>();
+        testStacks[0].addAll(Arrays.asList(3, 1, 4, 2, 5));
+
+        testStacks[1] = new Stack<>();
+        testStacks[1].addAll(Arrays.asList(10, 9, 8, 7, 6, 5));
+
+        testStacks[2] = new Stack<>();
+        testStacks[2].addAll(Arrays.asList(1, 2, 3, 4, 5));
+
+        testStacks[3] = new Stack<>();
+        testStacks[3].addAll(Arrays.asList(15));
+
+        testStacks[4] = new Stack<>();
+
+        for (int i = 0; i < testStacks.length; i++) {
+            System.out.println("\n--- Stack " + (i+1) + " ---");
+            displayStack(testStacks[i], "Original");
+
+
+            sortStack((Stack<Integer>) testStacks[i].clone());
+
+
+            Stack<Integer> recursiveStack = (Stack<Integer>) testStacks[i].clone();
+            sortStackRecursive(recursiveStack);
+            displayStack(recursiveStack, "Sorted (Recursive)");
+        }
+
+
+        System.out.println("\nComplexity Analysis:");
+        System.out.println("Iterative: O(n^2) worst case due to repeated moves.");
+        System.out.println("Recursive: O(n^2) worst case, but elegant.");
+        System.out.println("Space: O(n) for temporary stack or recursion call stack.");
+    }
+
+
+
+
+
+
 
 }
