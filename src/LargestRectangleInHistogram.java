9f5eb021afd2b211091ac9213b96ec3a3e9bcf69
alzahara <shahdaljabri33@gmail.com>
2026-04-28 09:14:49 +0400

    Brute force O(n^2) comparison

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -56,7 +56,18 @@ public class LargestRectangleInHistogram {
         }
         return maxArea;
     }
-
+    public static int bruteForceLargestRectangle(int[] heights) {
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int minHeight = heights[i];
+            for (int j = i; j < heights.length; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
+                int area = minHeight * (j - i + 1);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
 
     }
 }
